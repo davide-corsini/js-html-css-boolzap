500676487b3a06462d449b975192fca5f264b33d
davide <davidecorsinimilano@gmail.com>
2021-02-02 14:51:50 +0100

    add img and name with vuejs

---
+++ js/script.js
@@ -0,0 +1,155 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        activeContact:[
+            {
+                name: 'Davide',
+                src: 'img/davide.jpg',
+                visible: true
+            }
+        ],
+        contacts: [
+            {
+                name: 'Emanuele',
+                src: 'img/ema.jpg',
+                visible: true,
+                message: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'ciao',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'come',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'staiiii',
+                        status: 'sent'
+                    },
+                ]
+            },
+            {
+                name: 'Yuri',
+                src: 'img/yuri.jpg',
+                visible: true,
+                message: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'ciao',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'come',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'staiiii',
+                        status: 'sent'
+                    },
+                ]
+            },
+            {
+                name: 'Cristiano',
+                src: 'img/cri.jpg',
+                visible: true,
+                message: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'ciao',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'come',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'staiiii',
+                        status: 'sent'
+                    },
+                ]
+            },
+            {
+                name: 'Bif',
+                src: 'img/bif.jpg',
+                visible: true,
+                message: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'ciao',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'come',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'staiiii',
+                        status: 'sent'
+                    },
+                ]
+            },
+            {
+                name: 'Papaya',
+                src: 'img/papaya.jpg',
+                visible: true,
+                message: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'ciao',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'come',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'staiiii',
+                        status: 'sent'
+                    },
+                ]
+            },
+            {
+                name: 'Tina',
+                src: 'img/tina.jpg',
+                visible: true,
+                message: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'ciao',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'come',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'staiiii',
+                        status: 'sent'
+                    },
+                ]
+            },
+            {
+                name: 'Jack Teacher',
+                src: 'img/jack.jpg',
+                visible: true,
+                message: [
+
+                ]
+            }
+        ]
+    }
+    
+});
